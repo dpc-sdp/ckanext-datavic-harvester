2e2aa52c9cbc450b795b4c8f8b941806d7e7146e
Nathan Perry <nathan@salsadigital.com.au>
2019-10-21 15:37:26 +1300

    [DATAVIC-23] Process extra required fields.

---
+++ ckanext/datavic_harvester/bs4_helpers.py
@@ -0,0 +1,78 @@
+# Based on https://gist.github.com/revotu/21d52bd20a073546983985ba3bf55deb
+import re
+import requests
+
+from bs4 import BeautifulSoup
+
+# remove all attributes
+def _remove_all_attrs(soup):
+    for tag in soup.find_all(True):
+        tag.attrs = {}
+    return soup
+
+
+# remove all attributes except some tags
+def _remove_all_attrs_except(soup):
+    whitelist = ['a']
+    for tag in soup.find_all(True):
+        if tag.name not in whitelist:
+            tag.attrs = {}
+    return soup
+
+
+# remove all attributes except some tags(only saving ['href','src'] attr)
+def _remove_all_attrs_except_saving(soup):
+    whitelist = ['a']
+    for tag in soup.find_all(True):
+        if tag.name not in whitelist:
+            tag.attrs = {}
+        else:
+            attrs = dict(tag.attrs)
+            for attr in attrs:
+                if attr not in ['target','href']:
+                    del tag.attrs[attr]
+    return soup
+
+
+def _unwrap_all_except(soup, whitelist):
+    for tag in soup.find_all(True):
+        if tag.name not in whitelist:
+            tag.unwrap()
+    return soup
+
+
+def _extract_metadata_url(soup, base_url):
+    url = None
+    for tag in soup.find_all('a'):
+        if 'href' in tag.attrs and base_url in tag['href']:
+            url = tag['href']
+    return url
+
+
+def _fetch_update_frequency(full_metadata_url):
+    update_frequency = 'Unknown'
+    try:
+        response = requests.get(full_metadata_url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        for tag in soup('script', attrs={'id': 'tpx_ExternalView_Frequency_of_Updates'}):
+            tag_text = tag.get_text()
+            if 'deemed' in tag_text:
+                update_frequency = 'asNeeded'
+                break
+            elif 'week ' in tag_text:
+                update_frequency = 'weekly'
+                break
+            elif 'year' in tag_text:
+                update_frequency = 'annually'
+                break
+            elif 'month' in tag_text:
+                update_frequency = 'monthly'
+                break
+            elif 'quarter' in tag_text:
+                update_frequency = 'quarterly'
+                break
+    except Exception, e:
+        print("An error occured: %s" % str(e))
+
+    return update_frequency
--- ckanext/datavic_harvester/harvesters/_json.py
+++ ckanext/datavic_harvester/harvesters/_json.py
@@ -1,6 +1,12 @@
 import json
+
+from bs4 import BeautifulSoup
+from ckanext.datavic_harvester import bs4_helpers
+from ckan.common import config
 from ckanext.dcat import converters
 from ckanext.dcat.harvesters._json import DCATJSONHarvester
+from datetime import datetime
+from dateutil import tz
 
 
 class DataVicDCATJSONHarvester(DCATJSONHarvester):
@@ -13,14 +19,136 @@ class DataVicDCATJSONHarvester(DCATJSONHarvester):
                            'serialized as JSON'
         }
 
+    def convert_utc_datetime_to_local(self, utc_datetime):
+        try:
+            from_zone = tz.gettz('UTC')
+            to_zone = tz.gettz(config.get('ckan.display_timezone', 'Australia/Melbourne'))
+
+            utc = datetime.strptime(utc_datetime, '%Y-%m-%dT%H:%M:%S.%fZ')
+
+            # Tell the datetime object that it's in UTC time zone since
+            # datetime objects are 'naive' by default
+            utc = utc.replace(tzinfo=from_zone)
+
+            # Convert time zone
+            local_datetime = utc.astimezone(to_zone)
+            return local_datetime.date()
+        except Exception:
+            return utc_datetime
+
+    def assign_dcat_dates_to_dataset(self, dcat_dict, package_dict):
+        '''
+        Convert issued & modified dates to local timezone and assign to Data.Vic metadata fields
+        :param dcat_dict:
+        :param package_dict:
+        :return:
+        '''
+        if 'issued' in dcat_dict:
+            package_dict['extras'].append(
+                {'key': 'date_created_data_asset', 'value': self.convert_utc_datetime_to_local(dcat_dict['issued'])})
+
+        if 'modified' in dcat_dict:
+            package_dict['extras'].append(
+                {'key': 'date_modified_data_asset', 'value': self.convert_utc_datetime_to_local(dcat_dict['modified'])})
+
+        # Default group assignment
+        if 'groups' not in package_dict:
+            package_dict['groups'] = [{'name': 'transport'}]
+
+    def assign_datavic_required_fields(self, package_dict):
+        '''
+        Default Data.Vic mandatory field assignments
+        These would ideally be handled in an `edit` function on the IPackageController interface, but
+        there is already one in use on the ckanext-workflow extension which seems to block implementing on
+        the ckanext-datavicmain extension
+        :param package_dict:
+        :return:
+        '''
+        required_fields = [
+            {'key': 'personal_information', 'default': 'no'},
+            {'key': 'protective_marking', 'default': 'Public Domain'},
+            {'key': 'access', 'default': 'no'},
+            {'key': 'update_frequency', 'default': 'unknown'},
+        ]
+        for required_field in required_fields:
+            current_value = [extra for extra in package_dict['extras'] if extra['key'] == required_field['key']]
+            if not current_value:
+                package_dict['extras'].append({'key': required_field['key'], 'value': required_field['default']})
+
+    def fix_erroneous_tags(self, package_dict):
+        '''
+        Replace ampersands with "and" in tags
+        :param package_dict:
+        :return:
+        '''
+        if package_dict['tags']:
+            for tag in package_dict['tags']:
+                if 'name' in tag and '&' in tag['name']:
+                    tag['name'] = tag['name'].replace('&', 'and')
+
+    def generate_extract(self, soup):
+        '''
+        Extract is just the first sentence of the text-only description/notes for our purposes at this stage.
+        :param soup:
+        :return:
+        '''
+        try:
+            notes = soup.get_text()
+            index = notes.index('.')
+            notes = notes[:index + 1]
+        except Exception:
+            pass
+        return notes
+
     def _get_package_dict(self, harvest_object):
+        '''
+        Converts a DCAT dataset into a CKAN dataset
+        and performs some Data.Vic specific conversion of the data
+        :param harvest_object:
+        :return:
+        '''
 
         content = harvest_object.content
 
         dcat_dict = json.loads(content)
 
         package_dict = converters.dcat_to_ckan(dcat_dict)
-        
-        package_dict['extras'].append({'key': 'DATAVIC-23', 'value': 'check'})
+
+        soup = BeautifulSoup(package_dict['notes'], 'html.parser')
+
+        extract = [extra for extra in package_dict['extras'] if extra['key'] == 'extract']
+
+        if 'default.description' in package_dict['notes']:
+            package_dict['notes'] = 'No description has been entered for this dataset.'
+            if not extract:
+                package_dict['extras'].append({'key': 'extract', 'value': 'No abstract has been entered for this dataset.'})
+        else:
+            package_dict['notes'] = bs4_helpers._unwrap_all_except(
+                bs4_helpers._remove_all_attrs_except_saving(soup),
+                # allowed tags
+                ['a']
+            )
+            if not extract:
+                extract = self.generate_extract(soup)
+                if extract:
+                    package_dict['extras'].append({'key': 'extract', 'value': extract})
+
+        # Try and extract the full metadata URL from the dataset description
+        full_metadata_url = [extra for extra in package_dict['extras'] if extra['key'] == 'full_metadata_url']
+        if not full_metadata_url:
+            full_metadata_url = bs4_helpers._extract_metadata_url(soup, 'data.vicroads.vic.gov.au/metadata')
+            if full_metadata_url:
+                package_dict['extras'].append({'key': 'full_metadata_url', 'value': full_metadata_url})
+                # Attempt to extract the update frequency from the full metadata page
+                package_dict['extras'].append({
+                    'key': 'update_frequency',
+                    'value': bs4_helpers._fetch_update_frequency(full_metadata_url)
+                })
+
+        self.assign_dcat_dates_to_dataset(dcat_dict, package_dict)
+
+        self.assign_datavic_required_fields(package_dict)
+
+        self.fix_erroneous_tags(package_dict)
 
         return package_dict, dcat_dict
